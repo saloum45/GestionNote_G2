806b517f591d4f5b06a0c96c2c6a54d34023e104
Omar-ciber <omarsow388@gmail.com>
2023-11-13 17:13:57 +0000

    enregistrement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfesseursComponent } from './professeurs/professeurs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'professeurs', component:ProfesseursComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
