c4f5da992453fec160c813eefa0d5ee2eddc6a59
Omar-ciber <omarsow388@gmail.com>
2023-11-13 16:57:31 +0000

    enregistrement

---
+++ src/app/professeurs/professeurs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professeurs',
+  templateUrl: './professeurs.component.html',
+  styleUrls: ['./professeurs.component.scss']
+})
+export class ProfesseursComponent {
+
+}
