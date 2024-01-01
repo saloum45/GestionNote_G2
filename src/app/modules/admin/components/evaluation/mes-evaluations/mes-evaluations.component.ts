25f3ef6dabe1167426afad2c643493365e8454b4
saloum45 <saloumfall45@gmail.com>
2024-01-01 17:45:45 +0000

    in progress

--- src/app/modules/admin/components/evaluation/mes-evaluations/mes-evaluations.component.ts
+++ src/app/modules/admin/components/evaluation/mes-evaluations/mes-evaluations.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AllServiceService } from 'src/app/services/all-service.service';
+import { SweetMessageService } from 'src/app/services/sweet-message.service';
 
 @Component({
   selector: 'app-mes-evaluations',
   templateUrl: './mes-evaluations.component.html',
   styleUrls: ['./mes-evaluations.component.scss']
 })
-export class MesEvaluationsComponent {
+export class MesEvaluationsComponent implements OnInit {
+  // Attributs
+  public evaluationNote: any[]=[];
+  public notes: any;
+  public evaluations: any[]=[];
+  onlineProfesseur: any;
+  constructor(private service: AllServiceService, private message: SweetMessageService, private activatedRoute: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.onlineProfesseur = JSON.parse(localStorage.getItem('userOnline') || '[]');
+    this.service.getByForeignId("evaluations", this.onlineProfesseur.user[0].classeId, (reponse: any) => {
+      this.evaluations = reponse;
+      console.warn('ev', this.evaluations);
 
+    }, "classeId");
+    this.service.getByForeignId("notes", this.onlineProfesseur.user[0].id, (reponse: any) => {
+      this.notes = reponse;
+      console.warn('nt', this.notes);
+
+    }, "apprenantId");
+
+  }
 }
